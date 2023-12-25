99df722ef207d638a2c90c345b3485f59e5d07ce
RAM-413 <ramsharma22422@gmail.com>
2023-12-25 09:04:44 +0530

    update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,25 +2,35 @@ import React from "react";
 import imglogo from "../Images/Nav-logo.png";
 import imglogo1 from "../Images/nav-logo2.png";
 import { Container } from "react-bootstrap";
-import { Nav } from "react-bootstrap";
-
 
 export const Navbar = () => {
-  // // preloader
-    // setTimeout(() => {
-    //   preloader.classList.add("d-none");
-    //   document.body.classList.remove("overflow-hidden");
-    // }, 2000);
-
+  function openNav() {
+    document.getElementById("navBar").classList.toggle("start-0");
+    document.body.classList.toggle("overflow-hidden");
+    document.querySelector(".menu").classList.toggle("cross");
+  }
+  function closeNav() {
+    document.getElementById("navBar").classList.remove("start-0");
+    document.body.classList.remove("overflow-hidden");
+    document.querySelector(".menu").classList.remove("cross");
+  }
+  const preloader = document.getElementById("preloader");
+  setTimeout(() => {
+    document.getElementById("preloader").classList.add("d_none");
+    document.body.classList.remove("overflow-hidden");
+  }, 3000);
   return (
     <div>
-      {/* <!-- preloader --> */}
-      {/* <div
+      <div
         id="preloader"
-        class="min-vh-100 h-100 bg-black d-flex justify-content-center align-items-center position-fixed top-0 left-0 w-100 z-11 flex-column"
+        class=" overflow-y-hidden  position-relative loader_bg "
       >
-        <div class="loader">loading...</div>
-      </div> */}
+        <div class="bg-white z-19 vh-100 d-flex align-items-center justify-content-center  position-fixed  top-0 start-0 w-100 ">
+          <div class="d-flex align-items-center justify-content-center preloder-img ">
+            <div class="loader"></div>
+          </div>
+        </div>
+      </div>
       <header>
         <Container>
           <div className="d-lg-flex justify-content-between py-2 d-none ">
@@ -28,56 +38,97 @@ export const Navbar = () => {
             <img src={imglogo1} alt="Offer" className="d-none d-lg-block" />
           </div>
         </Container>
-        <Nav className="nav-bar py-29">
+        <div className="nav-bar py-lg-4 pb-2 ">
           <Container>
-            <div className=" d-flex align-items-center justify-content-between w-100">
-              <img src={imglogo} alt="" className=" d-block d-lg-none" />
-              <label for="menu-icon">
-                <span></span>
-                <span></span>
-                <span></span>
-              </label>
-              <input type="checkbox" id="menu-icon" hidden />
-              <ul className=" d-flex align-items-end gap-4 mobile-view m-0 p-0">
-                <li>
-                  <a className="header-links" href="#">
-                    HOME
-                  </a>
-                </li>
-                <li>
-                  <a className="header-links" href="#">
-                    SNEAKERS KOPEN
-                  </a>
-                </li>
-                <li>
-                  <a className="header-links" href="#">
-                    INTERVIEWS
-                  </a>
-                </li>
-                <li>
-                  <a className="header-links" href="#">
-                    SALE
-                  </a>
-                </li>
-                <li>
-                  <a className="header-links" href="#">
-                    SNEAKER VAN DE DAG
-                  </a>
-                </li>
-                <li>
-                  <a className="header-links" href="#">
-                    SNEAKER FORUM
-                  </a>
-                </li>
-                <li>
-                  <a className="header-links" href="#">
-                    BLOG
-                  </a>
-                </li>
-              </ul>
-            </div>
+            <nav>
+              <div className="d-flex align-items-cneter justify-content-between">
+                <div className=" d-flex  align-items-center position-relative ">
+                  <img
+                    className="w_100 d-block d-lg-none mt-2"
+                    src={imglogo}
+                    alt="#"
+                  />
+                </div>
+                <div
+                  onClick={openNav}
+                  className="menu d-block d-lg-none z-5 mt-4"
+                >
+                  <span className="nav-line1"></span>
+                  <span className="my-2 nav-line2"></span>
+                  <span className="nav-line3"></span>
+                </div>
+                <ul
+                  id="navBar"
+                  className="d-flex align-items-center gap-3 mb-0 mobileView"
+                >
+                  <li>
+                    <a
+                      onClick={closeNav}
+                      href="#HOME"
+                      className="text-black fs-s-md fw-normal header-links ff-roboto mb-0 position-relative common-h"
+                    >
+                      HOME
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      onClick={closeNav}
+                      href="#SNEAKERS"
+                      className=" text-black  fs-s-md fw-normal header-links ff-roboto mb-0 position-relative common-h"
+                    >
+                      SNEAKERS KOPEN
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      onClick={closeNav}
+                      href="#INTERVIEWS"
+                      className="text-black fs-s-md fw-normal header-links ff-roboto mb-0 position-relative common-h"
+                    >
+                      INTERVIEWS
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      onClick={closeNav}
+                      href="#SALE"
+                      className="text-black fs-s-md fw-normal header-links ff-roboto mb-0 position-relative common-h"
+                    >
+                      SALE
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      onClick={closeNav}
+                      href="#DAG"
+                      className="text-black fs-s-md fw-normal header-links ff-roboto mb-0 position-relative common-h"
+                    >
+                      SNEAKER VAN DE DAG
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      onClick={closeNav}
+                      href="#FORUM"
+                      className="text-black fs-s-md fw-normal header-links ff-roboto mb-0 position-relative common-h"
+                    >
+                      SNEAKER FORUM
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      onClick={closeNav}
+                      href="#BLOG"
+                      className="text-black fs-s-md fw-normal header-links ff-roboto mb-0 position-relative common-h"
+                    >
+                      BLOG
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
           </Container>
-        </Nav>
+        </div>
       </header>
     </div>
   );
